07d41a025a6839feea72f9824768d9f68d334429
yuvalmor <yuvalmor94@gmail.com>
2021-07-30 09:04:06 +0300

    wip

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ def get_image():
     return cv2.imread(image_name)
 
 
+def color_error_diffusion(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    error = np.zeros((height + 1, width + 1))
+    processed_image = np.zeros((height, width), "uint8")
+    for x in range(height):
+        for y in range(width):
+            processed_image[x, y] = get_closest_level_value(image[x, y] + error[x, y])
+            difference = (image[x, y] + error[x, y]) - processed_image[x, y]
+            error[x, y + 1] += difference * (3 / 8)
+            error[x + 1, y] += difference * (3 / 8)
+            error[x + 1, y + 1] += difference * (1 / 4)
+    return processed_image
+
+
 def get_closest_level_value(pixel_value):
     return LEVEL_VALUES[round(pixel_value / VALUES_GAP)]
 
@@ -22,30 +37,12 @@ def display_images(original_image, processed_image):
     cv2.destroyAllWindows()
 
 
-def color_error_diffusion(image):
-    height = image.shape[0]
-    width = image.shape[1]
-    error = np.zeros((height + 1, width + 1))
-    processed_image = np.zeros((height, width),"uint8")
-    for x in range(height):
-        for y in range(width):
-            processed_image[x,y] = get_closest_level_value(image[x,y])
-            difference = (image[x,y] + error[x,y]) - processed_image[x,y]
-            error[x,y+1] += int(difference*(3/8))
-            error[x+1,y] += int(difference*(3/8))
-            error[x+1,y+1] += int(difference*(1/4))
-    return processed_image
-
-
 if __name__ == '__main__':
     image = get_image()
-    image_b, image_g, image_r = cv2.split(image)
     processed_images = []
-    processed_image_b = color_error_diffusion(image_b)
-    processed_image_g = color_error_diffusion(image_g)
-    processed_image_r = color_error_diffusion(image_r)
-    merged_image = cv2.merge((processed_image_b,processed_image_g,processed_image_r))
-    #merged_image = merged_image.astype("uint8")
+    for channel in cv2.split(image):
+        processed_image = color_error_diffusion(channel)
+        processed_images.append(processed_image)
+    merged_image = cv2.merge(processed_images)
     display_images(image, merged_image)
-    filename = 'savedImage.jpg'
-    cv2.imwrite(filename, merged_image)
+    cv2.imwrite('processedImage.png', merged_image)
